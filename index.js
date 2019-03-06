5cdcc7c0fff65518bcab335e0dd1f9afbb098145
Dani Lyn <dlyn1110@gmail.com>
2019-03-06 20:46:43 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -54,7 +54,7 @@ function createIssue() {
     title: document.getElementById('title').value,
     body: document.getElementById('body').value
   }
-  const username = 'dlyn'
+  const username = ''
 
   fetch(`${baseURL}/repos/${username}/js-ajax-fetch-lab/issues`, {
     method: 'POST',
